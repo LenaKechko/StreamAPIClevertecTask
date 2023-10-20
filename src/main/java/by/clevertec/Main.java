c294e017257cec232e5c868d92fa9edf148a699f
Elena Kechko <ekechko@gmail.com>
2023-10-20 17:53:25 +0300

    Task4 done

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -14,28 +14,28 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
+//        task1();
+//        task2();
+//        task3();
         task4();
-        task5();
-        task6();
-        task7();
-        task8();
-        task9();
-        task10();
-        task11();
-        task12();
-        task13();
-        task14();
-        task15();
-        task16();
-        task17();
-        task18();
-        task19();
-        task20();
-        task21();
-        task22();
+//        task5();
+//        task6();
+//        task7();
+//        task8();
+//        task9();
+//        task10();
+//        task11();
+//        task12();
+//        task13();
+//        task14();
+//        task15();
+//        task16();
+//        task17();
+//        task18();
+//        task19();
+//        task20();
+//        task21();
+//        task22();
     }
 
     public static void task1() {
@@ -53,9 +53,13 @@ public class Main {
 //        animals.stream() Продолжить ...
     }
 
-    public static void task4() {
+    public static long task4() {
         List<Animal> animals = Util.getAnimals();
-//        animals.stream() Продолжить ...
+        long countFemaleAnimals = animals.stream()
+                .filter(animal -> animal.getGender().equals("Female"))
+                .count();
+        System.out.println(countFemaleAnimals);
+        return countFemaleAnimals;
     }
 
     public static void task5() {
