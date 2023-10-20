77369733fe3fb0f83e5b85e5891de53519f9f48f
Elena Kechko <ekechko@gmail.com>
2023-10-20 19:38:09 +0300

    Task9 done

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -9,33 +9,34 @@ import by.clevertec.model.Person;
 import by.clevertec.model.Student;
 import by.clevertec.util.Util;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
-        task4();
-        task5();
-        task6();
-        task7();
-        task8();
+//        task1();
+//        task2();
+//        task3();
+//        task4();
+//        task5();
+//        task6();
+//        task7();
+//        task8();
         task9();
-        task10();
-        task11();
-        task12();
-        task13();
-        task14();
-        task15();
-        task16();
-        task17();
-        task18();
-        task19();
-        task20();
-        task21();
-        task22();
+//        task10();
+//        task11();
+//        task12();
+//        task13();
+//        task14();
+//        task15();
+//        task16();
+//        task17();
+//        task18();
+//        task19();
+//        task20();
+//        task21();
+//        task22();
     }
 
     public static void task1() {
@@ -78,9 +79,13 @@ public class Main {
 //        animals.stream() Продолжить ...
     }
 
-    public static void task9() {
+    public static int task9() {
         List<Animal> animals = Util.getAnimals();
-//        animals.stream() Продолжить ...
+        int minLengthBread = animals.stream()
+                .map(animal -> animal.getBread().toCharArray().length)
+                .min(Integer::compareTo).get();
+        System.out.println(minLengthBread);
+        return minLengthBread;
     }
 
     public static void task10() {
