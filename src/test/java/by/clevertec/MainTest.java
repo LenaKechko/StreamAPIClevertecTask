3e49176e3035f7e7ad639f75ae975168542c7431
Elena Kechko <ekechko@gmail.com>
2023-10-23 12:03:37 +0300

    Task2 test

---
+++ src/test/java/by/clevertec/MainTest.java
@@ -0,0 +1,31 @@
+package by.clevertec;
+
+import by.clevertec.model.Animal;
+import by.clevertec.util.Util;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void task2() {
+        List<Animal> animals = Util.getAnimals();
+        List<String> expectedList = new ArrayList<>();
+        for (Animal animal : animals) {
+            if (animal.getOrigin().equals("Japanese") &&
+                    !(expectedList.contains(animal.getBread()) ||
+                            expectedList.contains(animal.getBread().toUpperCase()))) {
+                if (animal.getGender().equals("Female"))
+                    expectedList.add(animal.getBread().toUpperCase());
+                else {
+                    expectedList.add(animal.getBread());
+                }
+            }
+        }
+        assertEquals(expectedList, Main.task2());
+    }
+}
